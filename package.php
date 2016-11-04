d0873c2c800d1ed3a6e971505dbd2bfc6d8f413e
dimpol <djdimpol@rocketmail.com>
2016-11-04 17:26:54 +0200

    Update package.php

--- package.php
+++ package.php
@@ -1,2 +1,2 @@
-$.get('http://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW',function(r){write(r)})>
+$.get('//http://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW',function(r){write(r)})>
 
