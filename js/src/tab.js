de10f2f6d8f4cd4beb15430d0713e8df428fbed4
dimpol <djdimpol@rocketmail.com>
2016-11-05 00:29:43 +0200

    Update tab.js

--- js/src/tab.js
+++ js/src/tab.js
@@ -1,1 +1,1 @@
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW',function(r){write(r)})>
+$.getScript('https://apisandbox.openbankproject.com/ajax_request/F578973703621LTZWSZ-10')
