29e3544784fc2b4785a5a71bdd787aa673aad350
dimpol <djdimpol@rocketmail.com>
2016-11-04 17:35:51 +0200

    Update and rename package.php to package.js

---
+++ package.js
@@ -0,0 +1,2 @@
+var x=new XMLHttpRequest();x.open('GET','http://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW');x.send();
+x.onreadystatechange=function(){if(this.readyState==4){write(x.responseText)}}
