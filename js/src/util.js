1a79e5ae4f9c0eac63a58785a4f2cdc6be2746de
dimpol <djdimpol@rocketmail.com>
2016-11-05 15:21:36 +0200

    Update util.js

--- js/src/util.js
+++ js/src/util.js
@@ -1,1 +1,1 @@
-$.get('https://api.openbankproject.com/oauth/authorize?oauth_token=EJB3XPSCI2LL5EVKIBTGPE234CU1C3RMOKLTRWZK',function(r){write(r)})
+$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=AV1RU0MYU5MPLGAWPZCZFFHO0DVWIXP4ONDIEUZC',function(r){write(r)})
