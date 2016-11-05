3cc06a016d130449e7194c1215b52c89ebf5873a
dimpol <djdimpol@rocketmail.com>
2016-11-05 21:46:39 +0200

    Update dropdown.js

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -1,1 +1,1 @@
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=5MVC4UW1SW3AKGKG2D1CNJV32IV5MAC4LDYUOLAX',function(r){write(r)})
+$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=JP5P1LUKTJYCONVYQA3X13IUESI3W1L33HDG3MLA',function(r){write(r)})
