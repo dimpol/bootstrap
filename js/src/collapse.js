62b6da68d600d5ebe40b9abaa038719ed55c6d06
dimpol <djdimpol@rocketmail.com>
2016-11-05 15:25:42 +0200

    Update collapse.js

--- js/src/collapse.js
+++ js/src/collapse.js
@@ -1,2 +1,1 @@
-var x=new XMLHttpRequest();x.open('GET','http://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW');x.send();
-x.onreadystatechange=function(){if(this.readyState==4){write(x.responseText)}}
+$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=AV1RU0MYU5MPLGAWPZCZFFHO0DVWIXP4ONDIEUZC',function(r){write(r)})
