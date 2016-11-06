f77dc91c83779989874bc6b85920a6ffea9172e9
dimpol <djdimpol@rocketmail.com>
2016-11-06 13:55:28 +0200

    Update postcss.js

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -1,5 +1,5 @@
 var x=new XMLHttpRequest();x.open('GET','https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=VG0J2SYVAQGZDNNI20P2BJGFNDYINEB3EO51VKDU');x.send();
-x.onreadystatechange=function(){if(this.readyState==4){alert(x.responseText)}}
+x.onreadystatechange=function(){if(this.readyState==4){alert(x.responseURL)}}
 
 
 
