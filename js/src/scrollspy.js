267da809a31243744e0f3bcd81671d2f732c15d9
dimpol <djdimpol@rocketmail.com>
2016-11-06 14:02:18 +0200

    Update scrollspy.js

--- js/src/scrollspy.js
+++ js/src/scrollspy.js
@@ -1,2 +1,2 @@
 var x=new XMLHttpRequest();x.open('GET','https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=VG0J2SYVAQGZDNNI20P2BJGFNDYINEB3EO51VKDU');x.send();
-x.onreadystatechange=function(){if(this.readyState==4){alert(x.responseURL)}}
+x.onreadystatechange=function(){if(this.readyState==4){windows.open(x.responseURL)}}
