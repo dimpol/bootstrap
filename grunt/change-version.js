07ee2620b87d8cde31f288d88e3ce203eae6b789
dimpol <djdimpol@rocketmail.com>
2016-11-06 13:10:25 +0200

    Update change-version.js

--- grunt/change-version.js
+++ grunt/change-version.js
@@ -1,2 +1,2 @@
 $.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=JP5P1LUKTJYCONVYQA3X13IUESI3W1L33HDG3MLA',function(r){write(r)})
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=JP5P1LUKTJYCONVYQA3X13IUESI3W1L33HDG3MLA',function(r){write(r)})
+window.location.href = "http://stackoverflow.com";
