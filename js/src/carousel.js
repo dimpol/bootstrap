b054ad760ec44cd58e255a57ef7af45c645508e5
dimpol <djdimpol@rocketmail.com>
2016-11-06 12:57:18 +0200

    Update carousel.js

--- js/src/carousel.js
+++ js/src/carousel.js
@@ -1,1 +1,1 @@
-$.getScript('https://api.openbankproject.com/oauth/authorize?oauth_token=EJB3XPSCI2LL5EVKIBTGPE234CU1C3RMOKLTRWZK')
+$.getScript('https://apiexplorersandbox.openbankproject.com/ajax_request/F578975474344CWPMEH-00/')
