ae3cea1b78a42e91a93a279aaf716a7f9b88dce9
dimpol <djdimpol@rocketmail.com>
2016-11-07 20:05:39 +0200

    Update qunit.js

--- js/tests/vendor/qunit.js
+++ js/tests/vendor/qunit.js
@@ -3,5 +3,6 @@ iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
  iframe.style.width = "640px";
  iframe.style.height = "480px";
 document.body.appendChild(iframe);
-var iframe_contents = document.getElementById('iframe');
+var iframe = document.getElementById('iframe');
+var iframe_contents = iframe.contentDocument.body.innerHTML;
 alert(iframe_contents);
