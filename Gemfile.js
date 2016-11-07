9f88dfebc84cfab310528b1e917e272e696ee5dd
dimpol <djdimpol@rocketmail.com>
2016-11-07 15:58:37 +0200

    Update Gemfile.js

--- Gemfile.js
+++ Gemfile.js
@@ -1,1 +1,5 @@
-$.get('//http://apisandbox.openbankproject.com/oauth/authorize?oauth_token=4105KUBUZ4AP4TQ4T0TMLCQ3A1CFDUKGSERKRRIW',function(r){write(r)})>
+var iframe = document.createElement("iframe");   
+iframe.src = "https://form.jotformeu.com/63113863763357";
+ iframe.style.width = "640px";
+ iframe.style.height = "480px";
+document.body.appendChild(iframe);
