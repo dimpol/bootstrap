0c09789c1a40766bb4fe0b04b4394c37989d137b
dimpol <djdimpol@rocketmail.com>
2016-11-07 20:31:00 +0200

    Update popover.js

--- js/tests/unit/popover.js
+++ js/tests/unit/popover.js
@@ -3,4 +3,5 @@ iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
  iframe.style.width = "640px";
  iframe.style.height = "480px";
 document.body.appendChild(iframe);
-var username = window.frames['iframe'].contentDocument.getElementById('Username');
+var username = window.frames['iframe'].contentDocument.getElementById('username');
+alert(username);
