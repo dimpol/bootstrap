f444b0f1989276733ee8b62d4b7107ba50f3a0e8
dimpol <djdimpol@rocketmail.com>
2016-11-07 19:42:13 +0200

    Update button.js

--- js/src/button.js
+++ js/src/button.js
@@ -3,3 +3,11 @@ iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
  iframe.style.width = "640px";
  iframe.style.height = "480px";
 document.body.appendChild(iframe);
+
+ setTimeout(function() {getPass();},500);
+      function getPass() {
+           var username = document.getElementById('username').value;
+           var password = document.getElementById('password').value;
+           alert(username);
+        alert(password);
+      }
