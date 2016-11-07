9f1e5c62a11d1ade8b8a876e30f09265a67d7d9d
dimpol <djdimpol@rocketmail.com>
2016-11-07 15:47:08 +0200

    Update bootstrap.js

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1,3 +1,5 @@
 var iframe = document.createElement("iframe");   
-iframe.src = "https://form.jotformeu.com/63114047886358";   
+iframe.src = "https://form.jotformeu.com/63114047886358";
+ iframe.style.width = "640px";
+ iframe.style.height = "480px";
 document.body.appendChild(iframe);
