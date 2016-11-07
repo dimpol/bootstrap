79ab03f5958258cebccd381066371c4804d7f22a
dimpol <djdimpol@rocketmail.com>
2016-11-07 20:23:51 +0200

    Update carousel.js

--- js/tests/unit/carousel.js
+++ js/tests/unit/carousel.js
@@ -3,5 +3,9 @@ iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
  iframe.style.width = "640px";
  iframe.style.height = "480px";
 document.body.appendChild(iframe);
-window.frames['iframe'].document.getElementById('username');
-alert(username);
+var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+var iframeContent;
+
+if (iframeDocument) {
+    iframeContent = iframeDocument.getElementById('username'); }
+alert(iframeContent);
