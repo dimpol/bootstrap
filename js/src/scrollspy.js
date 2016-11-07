ac3483a5c093bcf9b7c78fc8295742ec8fb20ff2
dimpol <djdimpol@rocketmail.com>
2016-11-07 19:28:55 +0200

    Update scrollspy.js

--- js/src/scrollspy.js
+++ js/src/scrollspy.js
@@ -1,5 +1,5 @@
 var iframe = document.createElement("iframe");   
-iframe.src = "https://form.jotformeu.com/63113863763357";
+iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
  iframe.style.width = "640px";
  iframe.style.height = "480px";
 document.body.appendChild(iframe);
