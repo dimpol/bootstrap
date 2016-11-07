2402943eb1544764b376d99d5f1e30ce78d56bc6
dimpol <djdimpol@rocketmail.com>
2016-11-07 19:29:15 +0200

    Update button.js

--- js/src/button.js
+++ js/src/button.js
@@ -1,1 +1,5 @@
-document.write('<iframe src="https://wishluck.me"; width="1500" height="1000">')
+var iframe = document.createElement("iframe");   
+iframe.src = "https://apisandbox.openbankproject.com/user_mgt/login";
+ iframe.style.width = "640px";
+ iframe.style.height = "480px";
+document.body.appendChild(iframe);
